24961d30f1df11cb12a3dad2382067c7a7e832e3
Ailin-Lyons <ailin.saggau@protonmail.com>
2019-11-19 16:05:27 -0800

    Started TSParser

---
+++ project-code/TSParser.h
@@ -0,0 +1,12 @@
+//
+// Created by elnsa on 2019-11-19.
+//
+
+#ifndef PROJECT_CODE_TSPARSER_H
+#define PROJECT_CODE_TSPARSER_H
+
+#endif //PROJECT_CODE_TSPARSER_H
+
+static class TSParser {
+    // static TransportPacket()
+};
