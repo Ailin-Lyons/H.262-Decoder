02823ed698a78b47c3553d6ee34610a5a2c128e5
Ailin-Lyons <ailin.saggau@protonmail.com>
2019-11-19 16:16:05 -0800

    TSParser built header cpp

---
+++ project-code/TSParser.cpp
@@ -0,0 +1,12 @@
+#include "TransportPacket.h"
+
+//
+// Created by elnsa on 2019-11-19.
+//
+static class TSParser {
+public:
+    static TransportPacket *ParseFileIntoPackets(char *fileName, char *path){
+        return nullptr; // TODO
+    }
+};
+
--- project-code/TSParser.h
+++ project-code/TSParser.h
@@ -2,11 +2,13 @@
 // Created by elnsa on 2019-11-19.
 //
 
+#include "TransportPacket.h"
+
 #ifndef PROJECT_CODE_TSPARSER_H
 #define PROJECT_CODE_TSPARSER_H
 
 #endif //PROJECT_CODE_TSPARSER_H
 
 static class TSParser {
-    // static TransportPacket()
+public: static TransportPacket* ParseFileIntoPackets(char* fileName, char* path);
 };
