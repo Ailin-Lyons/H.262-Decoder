bcaf24cdd4abe8f99032faf9ef780adfb8d2e4e9
Ailin-Lyons <ailin.saggau@protonmail.com>
2019-11-22 18:19:39 -0800

    Added struct in TransportPacket, added comments

--- project-code/PacketParsers/TSParser.cpp
+++ project-code/PacketParsers/TSParser.cpp
@@ -15,10 +15,11 @@ class TSParser {
     int num_packets;
     char *file_buffer;
     int index = 0;
+
+public:
     /**
      * Constructor
      */
-public:
     TSParser(char *relative_path) {
         rf = new ifstream(relative_path, ios::in | ios::binary);
         file_size = getFileSize(relative_path);
@@ -34,13 +35,18 @@ public:
         file_buffer = new char[188];
     }
 
+    /**
+     * Must be called before calling getNextPacket to avoid errors
+     * @return true iff current there are additional packets in file
+     */
     bool HasNextPacket() {
         return index < num_packets - 1;
     }
 
     /**
-     *
-     * @return
+     * HasNextPacket must be called and return true before calling this function
+     * Returns the next TS Stream packet from the file as a TransportPacket object
+     * @return a TransportPacket object || a null pointer if there was an error loading packet
      */
     TransportPacket *GetNextPacket() {
         index++;
@@ -61,6 +67,11 @@ public:
 
 
 private:
+    /**
+     * A helper function that determines the length of a file in bytes
+     * @param relative_path of the file
+     * @return length of file in bytes || -1 if error
+     */
     int getFileSize(char *relative_path) {
         struct stat results;
 
@@ -69,6 +80,11 @@ private:
         else return -1;
     }
 
+    /**
+     * A helper function that verifies that the input file is valid
+     * @param relative_path path of the input file
+     * @return true iff file exists and is multiple of 188 bytes
+     */
     bool isValidFile(char *relative_path) {
         int file_size = TSParser::getFileSize(relative_path);
         if (file_size == -1) {
@@ -80,29 +96,35 @@ private:
         }
     }
 
+    /**
+     * helper function that creates a TransportPacket object given a TS packet in binary data
+     * @param packet: TS Packet in binary form
+     * @return a TransPortPacket Object containing all fields and data from binary packet
+     */
     TransportPacket buildTransportPacket(char *packet) {
-        unsigned char sync_byte = packet[0];
-        unsigned char transport_error_indicator = (packet[1] >> 7) & 0x1;
-        unsigned char payload_unit_start_indicator = packet[1] >> 6 & 0x1;
-        unsigned char transport_priority = packet[1] >> 5 & 0x1;
-        unsigned short pid = packet[2] + ((packet[1] & 0x1F) << 8);
-        unsigned char transport_scrambling_control = packet[3] >> 6 & 0x3;
-        unsigned char adaptation_field_control = packet[3] >> 4 & 0x3;
-        unsigned char continuity_counter = packet[3] & 0xF;
+        TransportPacket::transport_header_fields thf_out;
+        thf_out.sync_byte = packet[0];
+        thf_out.transport_error_indicator = (packet[1] >> 7) & 0x1;
+        thf_out.payload_unit_start_indicator = packet[1] >> 6 & 0x1;
+        thf_out.transport_priority = packet[1] >> 5 & 0x1;
+        thf_out.pid = TransportPacket::getPID(packet[2] + ((packet[1] & 0x1F) << 8));
+        thf_out.transport_scrambling_control = TransportPacket::getTSC(packet[3] >> 6 & 0x3);
+        thf_out.adaptation_field_control = TransportPacket::getAFC(packet[3] >> 4 & 0x3);
+        thf_out.continuity_counter = packet[3] & 0xF;
         char *index = (char *) &(packet[4]);
         AdaptationField *adaptationField;
-        if (adaptation_field_control == 0x2 || adaptation_field_control == 0x3) {
+        if (thf_out.adaptation_field_control == TransportPacket::AFC::AFieldOnly ||
+            thf_out.adaptation_field_control == TransportPacket::AFC::AFieldPayload) {
             index = AFParser::generateAdaptationField(&packet[4], adaptationField);
         }
         char *data = (char *) malloc(sizeof(char) * (188 - (index - packet)));
-        if (adaptation_field_control == 0x1 || adaptation_field_control == 0x3) {
+        if (thf_out.adaptation_field_control == TransportPacket::AFC::AFieldPayload ||
+            thf_out.adaptation_field_control == TransportPacket::AFC::PayloadOnly) {
             for (int i = 0; i < (188 - (index - packet)); i++) {
                 data[i] = index[i];
             }
         }
-        return TransportPacket(sync_byte, transport_error_indicator, payload_unit_start_indicator,
-                               transport_priority, pid, transport_scrambling_control, adaptation_field_control,
-                               continuity_counter, adaptationField, data);
+        return TransportPacket(thf_out, adaptationField, data);
     }
 };
 
--- project-code/Transport Packet Structure/TransportPacket.cpp
+++ project-code/Transport Packet Structure/TransportPacket.cpp
@@ -6,23 +6,26 @@
 #include "TransportPacket.h"
 
 
-TransportPacket::TransportPacket(unsigned char sb, unsigned char tei, unsigned char pusi, unsigned char tp,
-                                 unsigned short pid, unsigned char tsc, unsigned char afc, unsigned char cc, AdaptationField* af, char* d) {
-    sync_byte = sb;
-    transport_error_indicator = tei;
-    payload_unit_start_indicator = pusi;
-    transport_priority = tp;
-    this->pid = getPID(pid);
-    transport_scrambling_control = getTSC(tsc);
-    adaptation_field_control = getAFC(afc);
-    continuity_counter = cc;
+TransportPacket::TransportPacket(transport_header_fields thf, AdaptationField *af,
+                                 char *d) {
+    header_fields.sync_byte = thf.sync_byte;
+    header_fields.transport_error_indicator = thf.transport_error_indicator;
+    header_fields.payload_unit_start_indicator = thf.payload_unit_start_indicator;
+    header_fields.transport_priority = thf.transport_priority;
+    header_fields.transport_scrambling_control = thf.transport_scrambling_control;
+    header_fields.adaptation_field_control = thf.adaptation_field_control;
+    header_fields.continuity_counter = thf.continuity_counter;
     adaptationField = nullptr;
-    adaptationField = af;
+    adaptationField = af;    //TODO: adaptationField initialization
     data = d;
-    std::printf("sync: %hhx, tei: %hhx, pusi: %hhx, tp: %hhx, pid: %hx, tsc: %hhx, afc: %hhx, cc: %hhx, afptr: %x, dataptr: %x\n",sb,tei,pusi,tp,this->pid,tsc,afc,cc,af,d);
-    //TODO: adaptationField initialization
+    std::printf(
+            "sync: %hhx, tei: %hhx, pusi: %hhx, tp: %hhx, pid: %hx, tsc: %hhx, afc: %hhx, cc: %hhx, afptr: %x, dataptr: %x\n",
+            header_fields.sync_byte, header_fields.transport_error_indicator,
+            header_fields.payload_unit_start_indicator, header_fields.transport_priority,
+            header_fields.pid, header_fields.transport_scrambling_control, header_fields.adaptation_field_control,
+            header_fields.continuity_counter, af, d);
 }
 
 TransportPacket::~TransportPacket() = default;// {
-    //TODO: free the adaptationField resources
+//TODO: free the adaptationField resources
 //}
