0422bf57ce003725f2e3373b0ea14a24ad2f354d
Bhavesh Singhal <bhaveshsinghal007@gmail.com>
2019-12-23 13:19:58 +0530

    BitManipulatorTest.cpp finished

--- project-code/Util/BitManipulator.cpp
+++ project-code/Util/BitManipulator.cpp
@@ -1,16 +1,18 @@
 //
 // Created by elnsa on 2019-12-03.
 //
+
+#include<iostream>
 class BitManipulator {
 public:
     /**
      * Reads numBits bits starting at index and returns them as a long long
      * If numBits is larger than sizeof(long long) this will return the numBits lower order digits
      * @param address: address from which to start reading
-     * @param numBits: positive number of bits to be read
+     * @param numBits: number of bits to be read
      * @return a long long containing number in lowest n bits
      */
-    static long long ReadNBits(unsigned char *address, int numBits) {
+    static long long ReadNBits(unsigned char *address, unsigned int numBits) {
         unsigned long long out = 0;
         int i; // declared here as it is used as address in second loop body
         for (i = 0; i < numBits / 8; i++) {
@@ -30,7 +32,7 @@ public:
      * @param offset: 0-7: bits to offset from index
      * @returna a long long containing number in lowest n bits
      */
-    static long long ReadNBitsOffset(unsigned char *address, int offset, int numBits) {
+    static long long ReadNBitsOffset(unsigned char *address, unsigned int offset, unsigned int numBits) {
         unsigned long long out = 0;
         for (int o = offset; o < 8; o++) {
             out = (out << 1) + ((address[0] >> (7 - o)) & 0x1);
