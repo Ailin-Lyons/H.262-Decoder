878cc92f55c81c6af5efdc36b9e4b51a7761d3c4
Bhavesh Singhal <bhaveshsinghal007@gmail.com>
2020-01-18 14:32:35 -0800

    stashing more changes

--- project-code/AllTests/PacketParsersTest/ESPacketParserTest/SeqExtensionParserTest.cpp
+++ project-code/AllTests/PacketParsersTest/ESPacketParserTest/SeqExtensionParserTest.cpp
@@ -9,19 +9,25 @@
 
 TEST(AllTest, SequenceExtension_Parser_Test) {
     SequenceExtensionPacket::initializerStruct init{};
-
+    init.e_type = ExtensionPacket::extension_type::sequence;
+    init.profile_and_level_indication = 0x46;
+    init.progressive_sequence = true;
+    init.chroma_format = 0x2;
+    init.horizontal_size_extension = 0x0;
+    init.vertical_size_extension = 0x0;
+    init.bit_rate_extension = 0x0;
+    init.vbv_buffer_size_extension = 0x0;
+    init.low_delay = true;
+    init.frame_rate_extension_n = 0;
+    init.frame_rate_extension_d = 0;
     SequenceExtensionPacket expected = SequenceExtensionPacket(init);
-    char relative_path[] = R"(..\..\..\test files/Single Packets/testvideo_noaudio.ts)";
+    char relative_path[] = R"(..\..\..\test files\testvideo_noaudio.ts)";
     FileInterface::getInstance()->setInstance(relative_path);
     ESParser::getInstance()->initiateStream();
-    while (1) {
-        if (ESPacket::getStartCode(ESParser::getInstance()->nextESPacketID()) ==
-            ESPacket::start_code::extension) {
-            ExtensionPacket *actual = (ExtensionPacket *) ESParser::getInstance()->getNextPacket();
-            if (actual->getExtensionType() == ExtensionPacket::extension_type::sequence) {
-                ASSERT_EQ(expected, *((SequenceExtensionPacket *) actual));
-                break;
-            }
-        }
+    while (ESPacket::getStartCode(ESParser::getInstance()->nextESPacketID()) !=
+           ESPacket::start_code::extension) {
+        ESParser::getInstance()->getNextPacket();
     }
+    auto *actual = (SequenceExtensionPacket *) ESParser::getInstance()->getNextPacket();
+    ASSERT_EQ(expected, *actual);
 }
