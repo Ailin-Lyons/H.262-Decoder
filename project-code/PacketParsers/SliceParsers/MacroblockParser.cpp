12ceda92b39c41275fd456c8dd519ff1e564c9e1
Ailin-Lyons <ailin.saggau@protonmail.com>
2020-01-19 23:27:56 -0800

    Macroblock first VLC

--- project-code/PacketParsers/SliceParsers/MacroblockParser.cpp
+++ project-code/PacketParsers/SliceParsers/MacroblockParser.cpp
@@ -3,18 +3,69 @@
 //
 #include "ESParser.h"
 #include "../../ESPackets/Slice/Macroblock.h"
+#include "MacroblockParser.h"
+
 
 #define read(n) (ESParser::getInstance()->popNBits((n)))
 #define peek(n) (ESParser::getInstance()->peekNBits((n)))
 
-class MacroblockParser {
-public:
-    /**
-     * Builds a Macroblock() from ESParser data, starting after the start_code/stream_id
-     * H.262 6.2.5.1
-     */
-    static Macroblock *getNextPacket(Macroblock* mb) {
-        read(1);
-        return nullptr; //TODO
+MacroblockParser::vlc MacroblockParser::table_b1[] = {{1,  1,  0b1}, //Excludes macroblock_escape as that is handled separately
+                                                      {3,  2,  0b011},
+                                                      {3,  3,  0b010},
+                                                      {4,  4,  0b0011},
+                                                      {4,  5,  0b0010},
+                                                      {5,  6,  0b00011},
+                                                      {5,  7,  0b00010},
+                                                      {7,  8,  0b0000111},
+                                                      {7,  9,  0b0000110},
+                                                      {8,  10, 0b00001011},
+                                                      {8,  11, 0b00001010},
+                                                      {8,  12, 0b00001001},
+                                                      {8,  13, 0b00001000},
+                                                      {8,  14, 0b00000111},
+                                                      {8,  15, 0b00000110},
+                                                      {10, 16, 0b0000010111},
+                                                      {10, 17, 0b0000010110},
+                                                      {10, 18, 0b0000010101},
+                                                      {10, 19, 0b0000010100},
+                                                      {10, 20, 0b0000010011},
+                                                      {10, 21, 0b0000010010},
+                                                      {11, 22, 0b00000100011},
+                                                      {11, 23, 0b00000100010},
+                                                      {11, 24, 0b00000100001},
+                                                      {11, 25, 0b00000100000},
+                                                      {11, 26, 0b00000011111},
+                                                      {11, 27, 0b00000011110},
+                                                      {11, 28, 0b00000011101},
+                                                      {11, 29, 0b00000011100},
+                                                      {11, 30, 0b00000011011},
+                                                      {11, 31, 0b00000011010},
+                                                      {11, 32, 0b00000011000},
+                                                      {11, 33, 0b00000001000}};
+
+Macroblock *MacroblockParser::getNextPacket(Macroblock *mb) {
+    Macroblock::initializerStruct init = {};
+    init.macroblock_address_increment = getAddressIncrement();
+    MacroblockModesParser::getNextPacket(init.macroBlockModes);
+    printf("mai = %u\n", init.macroblock_address_increment);
+    //TODO continue here
+    read(1);
+    return nullptr;
+}
+
+size_t MacroblockParser::getAddressIncrement() {
+    size_t out = 0;
+    while(peek(11)==0b00000001000){ //handle macroblock_escape
+        read(11);
+        out += 33;
     }
-};
+    for(vlc code: table_b1){
+        if(peek(code.numbits)==code.key){
+            unsigned char temp = read(code.value);
+            return out + code.value;
+        }
+    }
+    throw PacketException("MacroblockParser::getAddressIncrement: Unexpected valued\n");
+}
+
+
---
+++ project-code/PacketParsers/SliceParsers/MacroblockParser.h
@@ -0,0 +1,40 @@
+//
+// Created by elnsa on 2020-01-19.
+//
+
+#ifndef PROJECT_CODE_MACROBLOCKPARSER_H
+#define PROJECT_CODE_MACROBLOCKPARSER_H
+//
+// Created by elnsa on 2020-01-19.
+//
+#include "ESParser.h"
+#include "../../ESPackets/Slice/Macroblock.h"
+#include "MacroblockModesParser.cpp"
+
+#define read(n) (ESParser::getInstance()->popNBits((n)))
+#define peek(n) (ESParser::getInstance()->peekNBits((n)))
+
+class MacroblockParser {//TODO continue here
+private:
+    struct vlc {
+        unsigned char numbits;
+        unsigned char value;
+        unsigned short key;
+    };
+    static const size_t table_b1_size = 34;
+    static vlc table_b1[];
+
+public:
+/**
+     * Builds a Macroblock() from ESParser data, starting after the start_code/stream_id
+     * and stores it in mb
+     * H.262 6.2.5.1
+     */
+    static Macroblock *getNextPacket(Macroblock *mb);
+
+private:
+    static size_t getAddressIncrement();
+};
+
+
+#endif //PROJECT_CODE_MACROBLOCKPARSER_H
